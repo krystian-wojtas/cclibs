e5b11cd55009448c8f84cfb6d84c6916f7f517ca
Quentin King <quentin.king@cern.ch>
2013-12-16 12:17:56 +0000

    Refactor libfg ramp.h to refer to ramp not pp

--- libfg/inc/libfg/ramp.h
+++ libfg/inc/libfg/ramp.h
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------------------*\
-  File:     libfg/pp.h                                                                Copyright CERN 2014
+  File:     libfg/ramp.h                                                               Copyright CERN 2014
 
   License:  This file is part of libfg.
 
@@ -16,50 +16,50 @@
             You should have received a copy of the GNU Lesser General Public License
             along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-  Purpose:  Header file for pp.c : Parabola - Parabola functions
+  Purpose:  Header file for ramp.c : Fast ramp based on Parabola - Parabola function
 
   Contact:  cclibs-devs@cern.ch
 
-  Notes:    PP = Parabolic - Parabolic function with time shift when rate limited
+  Notes:    Parabolic - Parabolic function with time shift when rate limited
 
-            The PP function is special amongst all the functions in libfg because it can
+            The RAMP function is special amongst all the functions in libfg because it can
             uses the ref passed by reference for the previous iteration to adjust the function
             time. This allows a smooth parabolic end to the function, even if the function
             was rate limited by the calling application.
 
-            The fgPpGen function receives time as a pointer to constant double rather than as a double value.
+            The fgRampGen function receives time as a pointer to constant double rather than as a double value.
             This allows the user to initialise an array of pointers to functions with the pointer to the
-            fgPpGen function.  If time is passed by value then the compiler promotes the float to double
+            fgRampGen function.  If time is passed by value then the compiler promotes the float to double
             and prevents the correct initialisation.
 \*---------------------------------------------------------------------------------------------------------*/
 
-#ifndef LIBFG_PP_H
-#define LIBFG_PP_H
+#ifndef LIBFG_RAMP_H
+#define LIBFG_RAMP_H
 
 #include "libfg.h"
 
 // Constants
 
-#define FG_PP_N_SEGS          2             // Number of segments: P-P -> 2
+#define FG_RAMP_N_SEGS          2           // Number of segments: P-P -> 2
 
 // Types
 
-struct fg_pp_config                         // PP function configuration
+struct fg_ramp_config                       // RAMP function configuration
 {
     float       final;                      // Final reference
     float       acceleration;               // Absolute acceleration of the parabolic segments (must be strictly positive)
 };
 
-struct fg_pp_pars                           // PP function parameters
+struct fg_ramp_pars                         // RAMP function parameters
 {
-    uint32_t    pos_ramp_flag;              // Positive ramp flag (PP must be inverted for positive ramps)
+    uint32_t    pos_ramp_flag;              // Positive ramp flag (RAMP must be inverted for positive ramps)
     float       delay;                      // Time before start of function
     float       acceleration;               // Parabolic acceleration
     float       deceleration;               // Parabolic deceleration (for now this equals acceleration)
-    float       ref[FG_PP_N_SEGS+1];        // End of segment normalised references
-    float       time[FG_PP_N_SEGS+1];       // End of segment times
+    float       ref[FG_RAMP_N_SEGS+1];      // End of segment normalised references
+    float       time[FG_RAMP_N_SEGS+1];     // End of segment times
     float       offset;                     // Reference offset = 2.ref[2]
-    float       prev_pp_ref;                // Function ref from previous iteration
+    float       prev_ramp_ref;              // Function ref from previous iteration
     float       prev_returned_ref;          // Returned ref from previous iteration
     double      prev_time;                  // Time from previous iteration
     double      time_shift;                 // Time shift
@@ -71,12 +71,12 @@ extern "C" {
 
 // External functions
 
-void            fgPpCalc(struct fg_pp_config *config, struct fg_pp_pars *pars,
-                         float delay, float init_ref, struct fg_meta *meta);
-uint32_t        fgPpGen (struct fg_pp_pars *pars, const double *time, float *ref);
-enum fg_error   fgPpInit(struct fg_limits *limits, enum fg_limits_polarity limits_polarity,
-                         struct fg_pp_config *config, float delay, float ref,
-                         struct fg_pp_pars *pars, struct fg_meta *meta);
+void            fgRampCalc(struct fg_ramp_config *config, struct fg_ramp_pars *pars,
+                           float delay, float init_ref, struct fg_meta *meta);
+uint32_t        fgRampGen (struct fg_ramp_pars *pars, const double *time, float *ref);
+enum fg_error   fgRampInit(struct fg_limits *limits, enum fg_limits_polarity limits_polarity,
+                           struct fg_ramp_config *config, float delay, float ref,
+                           struct fg_ramp_pars *pars, struct fg_meta *meta);
 
 #ifdef __cplusplus
 }
