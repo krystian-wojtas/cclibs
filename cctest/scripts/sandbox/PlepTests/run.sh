ecd5113443f2c898eefaa5bb681bb9fd9524e764
Quentin King <quentin.king@cern.ch>
2014-02-04 22:13:11 +0100

    Add and update cctest PlepTest scripts

--- cctest/scripts/sandbox/PlepTests/run.sh
+++ cctest/scripts/sandbox/PlepTests/run.sh
@@ -11,5 +11,12 @@ source ../../run_header.sh
 "$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep03  > "$outpath/plep03.$file_type" 
 "$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep04  > "$outpath/plep04.$file_type" 
 "$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep05  > "$outpath/plep05.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep06  > "$outpath/plep06.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep07  > "$outpath/plep07.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep08  > "$outpath/plep08.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep09  > "$outpath/plep09.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep10  > "$outpath/plep10.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep11  > "$outpath/plep11.$file_type" 
+"$cctest" -o $output_format -g pars/global_10ms -f PLEP -d functions/plep12  > "$outpath/plep12.$file_type" 
 
 # EOF
