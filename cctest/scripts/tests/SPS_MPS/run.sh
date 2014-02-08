754a7b09fb261a08693a44ae8fe0936260477fcb
Quentin King <quentin.king@cern.ch>
2014-02-08 13:16:51 +0100

    Updated cctest SPS_MPS scripts to add CNGS table based on measurement and adjust load

--- cctest/scripts/tests/SPS_MPS/run.sh
+++ cctest/scripts/tests/SPS_MPS/run.sh
@@ -9,7 +9,8 @@ source ../../run_header.sh
 "$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/LHCFAST2_mb        -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-LHCFAST2_mb.$file_type" && 
 "$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/CNGS_400GeV_mb     -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-CNGS_400GeV_mb.$file_type" && 
 "$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/LHC_4INJ_450Gev_mb -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-LHC_4INJ_450Gev_mb.$file_type" && 
-"$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/SFTLONG_400Gev_mb  -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-SFTLONG_400Gev_mb.$file_type"  
+"$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/SFTLONG_400Gev_mb  -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-SFTLONG_400Gev_mb.$file_type" && 
+"$cctest" -o $output_format -g pars/amps/global -f TABLE -d functions/amps/CNGS-measured_mb   -m pars/limits -l pars/load_mb -s pars/vs -r pars/amps/reg  > "$outpath/amps-CNGS-measured_mb.$file_type"  
 ) || exit 1
 
 # EOF
