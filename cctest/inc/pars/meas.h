a7857bec44ada4dd85aca9b870dd468166cef1de
Quentin King <quentin.king@cern.ch>
2014-03-24 22:11:03 +0100

    Update cctest/inc/pars/meas.h to add extrapolate flags and FIR filters instead of IIR

--- cctest/inc/pars/meas.h
+++ cctest/inc/pars/meas.h
@@ -40,16 +40,23 @@ struct ccpars_meas
 {
     // Meas parameters
 
-    float                   v_delay_iters;    // Voltage measurement delay in iterations
-    float                   i_delay_iters;    // Current measurement delay in iterations
-    float                   b_delay_iters;    // Field   measurement delay in iterations
+    float                   v_delay_iters;              // Voltage measurement delay in iterations
+    float                   i_delay_iters;              // Current measurement delay in iterations
+    float                   b_delay_iters;              // Field   measurement delay in iterations
 
-    struct reg_meas_pars    i_meas_pars;      // Current measurement IIR filter parameters
-    struct reg_meas_pars    b_meas_pars;      // Field measurement IIR filter parameters
+    uint32_t                i_extrapolate;              // Extrapolate current measurement for regulation
+    uint32_t                b_extrapolate;              // Extrapolate field   measurement for regulation
 
-    float                   v_sim_noise_pp;   // Simulated voltage measurement peak-peak noise level
-    float                   i_sim_noise_pp;   // Simulated current measurement peak-peak noise level
-    float                   b_sim_noise_pp;   // Simulated field   measurement peak-peak noise level
+    uint32_t                i_fir_orders[2];            // Current measurement FIR filter orders
+    uint32_t                b_fir_orders[2];            // Field   measurement FIR filter orders
+
+    float                   v_sim_noise_pp;             // Simulated voltage measurement peak-peak noise level
+    float                   i_sim_noise_pp;             // Simulated current measurement peak-peak noise level
+    float                   b_sim_noise_pp;             // Simulated field   measurement peak-peak noise level
+
+    uint32_t                tone_half_period_iters;     // Simulated measurement tone half-period in iterations
+    float                   i_sim_tone_amp;             // Simulated current measurement tone amplitude
+    float                   b_sim_tone_amp;             // Simulated field   measurement tone amplitude
 };
 
 CCPARS_MEAS_EXT struct ccpars_meas ccpars_meas
@@ -58,11 +65,16 @@ CCPARS_MEAS_EXT struct ccpars_meas ccpars_meas
     1.3,                                      // V_DELAY_ITERS
     1.3,                                      // I_DELAY_ITERS
     1.3,                                      // B_DELAY_ITERS
-    {  { 1.0 }, { 1.0 }  },                   // I_MEAS_NUM, I_MEAS_DEN: Default current measurement IIR filter
-    {  { 1.0 }, { 1.0 }  },                   // B_MEAS_NUM, B_MEAS_DEN: Default field measurement IIR filter
+    CC_DISABLED,                              // I_EXTRAPOLATE
+    CC_DISABLED,                              // B_EXTRAPOLATE
+    { 1.0, 1.0 },                             // I_FIR_ORDERS
+    { 1.0, 1.0 },                             // B_FIR_ORDERS
     0.0,                                      // I_SIM_NOISE_PP
     0.0,                                      // I_SIM_NOISE_PP
     0.0,                                      // B_SIM_NOISE_PP
+    100,                                      // TONE_HALF_PERIOD_ITERS
+    0.0,                                      // I_SIM_TONE_AMP
+    0.0,                                      // B_SIM_TONE_AMP
 }
 #endif
 ;
@@ -71,17 +83,19 @@ CCPARS_MEAS_EXT struct ccpars_meas ccpars_meas
 
 CCPARS_MEAS_EXT struct ccpars meas_pars_list[]
 #ifdef GLOBALS
-= {// "Signal name"      TYPE,max_vals,min_vals,*enum,  *value,                       num_defaults
-    { "V_DELAY_ITERS",   PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.v_delay_iters   }, 1 },
-    { "I_DELAY_ITERS",   PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.i_delay_iters   }, 1 },
-    { "B_DELAY_ITERS",   PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.b_delay_iters   }, 1 },
-    { "I_MEAS_NUM",      PAR_FLOAT, REG_N_IIR_COEFFS, 0, NULL, { .f =  ccpars_meas.i_meas_pars.num }, 1 },
-    { "I_MEAS_DEN",      PAR_FLOAT, REG_N_IIR_COEFFS, 0, NULL, { .f =  ccpars_meas.i_meas_pars.den }, 1 },
-    { "B_MEAS_NUM",      PAR_FLOAT, REG_N_IIR_COEFFS, 0, NULL, { .f =  ccpars_meas.b_meas_pars.num }, 1 },
-    { "B_MEAS_DEN",      PAR_FLOAT, REG_N_IIR_COEFFS, 0, NULL, { .f =  ccpars_meas.b_meas_pars.den }, 1 },
-    { "V_SIM_NOISE_PP",  PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.v_sim_noise_pp  }, 1 },
-    { "I_SIM_NOISE_PP",  PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.i_sim_noise_pp  }, 1 },
-    { "B_SIM_NOISE_PP",  PAR_FLOAT,  1,               0, NULL, { .f = &ccpars_meas.b_sim_noise_pp  }, 1 },
+= {// "Signal name"            TYPE,max_vals,min_vals,*enum,          *value,                               num_defaults
+    { "V_DELAY_ITERS",         PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.v_delay_iters          }, 1 },
+    { "I_DELAY_ITERS",         PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.i_delay_iters          }, 1 },
+    { "B_DELAY_ITERS",         PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.b_delay_iters          }, 1 },
+    { "I_EXTRAPOLATE",         PAR_ENUM,     1, 0, enabled_disabled,{ .i = &ccpars_meas.i_extrapolate          }, 1 },
+    { "I_FIR_ORDERS",          PAR_FLOAT,    2, 0, NULL,            { .f =  ccpars_meas.i_fir_orders           }, 2 },
+    { "B_FIR_ORDERS",          PAR_FLOAT,    2, 0, NULL,            { .f =  ccpars_meas.b_fir_orders           }, 2 },
+    { "V_SIM_NOISE_PP",        PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.v_sim_noise_pp         }, 1 },
+    { "I_SIM_NOISE_PP",        PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.i_sim_noise_pp         }, 1 },
+    { "B_SIM_NOISE_PP",        PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.b_sim_noise_pp         }, 1 },
+    { "TONE_HALF_PERIOD_ITERS",PAR_UNSIGNED, 1, 0, NULL,            { .i = &ccpars_meas.tone_half_period_iters }, 1 },
+    { "I_SIM_TONE_AMP",        PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.i_sim_tone_amp         }, 1 },
+    { "B_SIM_TONE_AMP",        PAR_FLOAT,    1, 0, NULL,            { .f = &ccpars_meas.b_sim_tone_amp         }, 1 },
     { NULL }
 }
 #endif
