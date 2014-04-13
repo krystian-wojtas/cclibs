6feb0eb59c8a94b564fa949fd651bf2861654ae0
Quentin King <quentin.king@cern.ch>
2014-04-13 11:59:13 +0200

    Add cctest/inc/ccInit.h

---
+++ cctest/inc/ccInit.h
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------------------*\
+  File:     cctest/inc/ccInit.h                                                         Copyright CERN 2014
+
+  License:  This file is part of cctest.
+
+            cctest is free software: you can redistribute it and/or modify
+            it under the terms of the GNU Lesser General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+
+            This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU Lesser General Public License for more details.
+
+            You should have received a copy of the GNU Lesser General Public License
+            along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+  Purpose:  Header file for ccInit.c
+
+  Authors:  Quentin.King@cern.ch
+\*---------------------------------------------------------------------------------------------------------*/
+
+#ifndef CCINIT_H
+#define CCINIT_H
+
+// Function declarations
+
+void ccInitLoad       (void);
+void ccInitLimits     (void);
+void ccInitFunction   (void);
+void ccInitSimulation (void);
+void ccInitRegulation (void);
+
+#endif
+// EOF
