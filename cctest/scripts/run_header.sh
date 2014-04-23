9d0f2be4998aedda607600fa7b20585ffdb82217
Quentin King <quentin.king@cern.ch>
2014-04-23 20:38:19 +0200

    Update cctest/scripts/run_header.sh to work with new cctest

--- cctest/scripts/run_header.sh
+++ cctest/scripts/run_header.sh
@@ -2,33 +2,15 @@
 # Script used in all run.sh scripts
 #
 if [ $# -eq 0 ]; then
-  output_format=FLOT
+  csv_format=NONE
 elif [ $# -eq 1 ]; then
-  output_format=$1
+  csv_format=$1
 else
-  echo usage: run.sh [OUTPUT_FORMAT]
+  echo usage: run.sh [CSV_FORMAT]
   exit -1
-fi
-
-if [ $output_format = FLOT ]; then
-  output_dir=webplots
-  file_type=html
-else
-  output_dir=csv
-  file_type=csv
 fi
 
 cctest=../../../`uname -s`/`uname -m`/cctest
 
-dir=`pwd`
-dirpath=`dirname "$dir"`
-group=`basename "$dirpath"`
-project=`basename "$dir"`
-results_path=../../../results
-outpath="$results_path/$output_dir/$group/$project"
-rm -f "$outpath"/*.$file_type
-mkdir -p "$outpath"
-
-echo CCTEST - $output_format - $group - $project - $outpath
 set -x
 # EOF
