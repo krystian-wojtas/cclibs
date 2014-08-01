81005217e621be63a2d547b6c25a1c2efe5895d4
Quentin King <quentin.king@cern.ch>
2014-08-01 17:29:50 +0200

    Update mkcleanall.sh and .gitignore

--- mkcleanall.sh
+++ mkcleanall.sh
@@ -1,9 +1,25 @@
 #!/bin/sh
 
-for makefile in `find . -type f -maxdepth 2 -name Makefile`
+# make libs
+
+for makefile in `find lib* -maxdepth 1 -type f -name Makefile`
 do
     cd `dirname $makefile`
+    echo -e "\n\n##################################################"
     pwd
+    echo -e "##################################################\n\n"
+    make clean all || exit 1
+    cd -
+done
+
+# make test programs
+
+for makefile in `find *test ccrt -maxdepth 1 -type f -name Makefile`
+do
+    cd `dirname $makefile`
+    echo -e "\n\n##################################################"
+    pwd
+    echo -e "##################################################\n\n"
     make clean all || exit 1
     cd -
 done
